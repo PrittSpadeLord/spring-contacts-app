5dae6bd81ef3e9265043d08e505838b1d00417a6
PrittSpadeLord <pritt1999@gmail.com>
2025-12-30 18:58:32 +0530

    updated password deserializer to prevent misuse on non-applicable fields

--- src/main/java/io/github/prittspadelord/application/rest/support/PasswordDeserializer.java
+++ src/main/java/io/github/prittspadelord/application/rest/support/PasswordDeserializer.java
@@ -2,7 +2,6 @@ package io.github.prittspadelord.application.rest.support;
 
 import org.bouncycastle.util.Arrays;
 
-import tools.jackson.core.JacksonException;
 import tools.jackson.core.JsonParser;
 import tools.jackson.core.JsonToken;
 import tools.jackson.databind.DeserializationContext;
@@ -11,14 +10,14 @@ import tools.jackson.databind.ValueDeserializer;
 public class PasswordDeserializer extends ValueDeserializer<char[]> {
 
     @Override
-    public char[] deserialize(JsonParser p, DeserializationContext ctxt) throws JacksonException {
+    public char[] deserialize(JsonParser p, DeserializationContext ctxt) {
         if (p.currentToken() == JsonToken.VALUE_NULL) return null;
 
         char[] sourceBuffer = p.getStringCharacters();
         int offset = p.getStringOffset();
         int length = p.getStringLength();
 
-        //Make sure to add exception handling if the DeserializationContext type isnt char[]!
+        if(!ctxt.getContextualType().hasRawClass(char[].class)) throw new RuntimeException("Fields has PasswordDeserializer annotated despite not being char[] but instead a " + ctxt.getContextualType().getRawClass().getName());
 
         return Arrays.copyOfRange(sourceBuffer, offset, offset + length);
     }
