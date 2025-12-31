afc2a348c11aef94974fde69692dade6da3fc61a
PrittSpadeLord <pritt1999@gmail.com>
2026-01-01 00:56:36 +0530

    added contacts repository

--- src/main/java/io/github/prittspadelord/application/data/dao/ContactDao.java
+++ src/main/java/io/github/prittspadelord/application/data/dao/ContactDao.java
@@ -7,7 +7,6 @@ import lombok.extern.slf4j.Slf4j;
 
 import org.springframework.jdbc.core.BeanPropertyRowMapper;
 import org.springframework.jdbc.core.RowMapper;
-import org.springframework.jdbc.core.namedparam.BeanPropertySqlParameterSource;
 import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 import org.springframework.jdbc.core.namedparam.SqlParameterSource;
@@ -66,7 +65,7 @@ public class ContactDao {
         SqlParameterSource parameterSource = new MapSqlParameterSource()
             .addValue("id", contact.getId())
             .addValue("user_id", contact.getUserId())
-            .addValue("name_prefix", contact.getNamePrefix())
+            .addValue("name_prefix", contact.getNamePrefix().name())
             .addValue("first_name", contact.getFirstName())
             .addValue("last_name", contact.getLastName())
             .addValue("home_phone_number_country_code", contact.getHomePhoneNumberCountryCode())
@@ -85,6 +84,18 @@ public class ContactDao {
         int rowsAffected = this.namedParameterJdbcTemplate.update(sql, parameterSource);
 
         log.info("Added contact with id {} for user with id {}, with {} rows affected", contact.getId(), contact.getUserId(), rowsAffected);
+    }
+
+    public void deleteContact(long id, long userId) {
+        String sql = "DELETE FROM contacts WHERE id = :id AND user_id = :user_id";
+
+        SqlParameterSource parameterSource = new MapSqlParameterSource()
+            .addValue("id", id)
+            .addValue("user_id", userId);
+
+        int rowsAffected = this.namedParameterJdbcTemplate.update(sql, parameterSource);
+
+        log.info("Deleted contact with id {} for user with id {}, with {} rows affected", id, userId, rowsAffected);
     }
 
     public Contact getContact(long id) {
@@ -153,6 +164,48 @@ public class ContactDao {
 
     public void updateContact(long id, Contact newContact) {
         String sql = """
-                """;
+            UPDATE contacts
+            SET
+                user_id = :user_id,
+                name_prefix = :name_prefix::name_prefix,
+                first_name = :first_name,
+                last_name = :last_name,
+                home_phone_number_country_code = :home_phone_number_country_code,
+                home_phone_number = :home_phone_number,
+                mobile_phone_number_country_code = :mobile_phone_number_country_code,
+                mobile_phone_number = :mobile_phone_number,
+                personal_email_address = :personal_email_address,
+                work_email_address = :work_email_address,
+                address_line1 = :address_line1,
+                address_line2 = :address_line2,
+                city = :city,
+                province = :province,
+                country = :country,
+                postal_code = :postal_code
+            WHERE id = :id
+            """;
+
+        SqlParameterSource parameterSource = new MapSqlParameterSource()
+            .addValue("id", newContact.getId())
+            .addValue("user_id", newContact.getUserId())
+            .addValue("name_prefix", newContact.getNamePrefix().name())
+            .addValue("first_name", newContact.getFirstName())
+            .addValue("last_name", newContact.getLastName())
+            .addValue("home_phone_number_country_code", newContact.getHomePhoneNumberCountryCode())
+            .addValue("home_phone_number", newContact.getHomePhoneNumber())
+            .addValue("mobile_phone_number_country_code", newContact.getMobilePhoneNumberCountryCode())
+            .addValue("mobile_phone_number", newContact.getMobilePhoneNumber())
+            .addValue("personal_email_address", newContact.getPersonalEmailAddress())
+            .addValue("work_email_address", newContact.getWorkEmailAddress())
+            .addValue("address_line1", newContact.getAddressLine1())
+            .addValue("address_line2", newContact.getAddressLine2())
+            .addValue("city", newContact.getCity())
+            .addValue("province", newContact.getProvince())
+            .addValue("country", newContact.getCountry())
+            .addValue("postal_code", newContact.getPostalCode());
+
+        int rowsAffected = this.namedParameterJdbcTemplate.update(sql, parameterSource);
+
+        log.info("Updated contact with id {} for user with id {}, with {} rows affected", newContact.getId(), newContact.getUserId(), rowsAffected);
     }
 }
